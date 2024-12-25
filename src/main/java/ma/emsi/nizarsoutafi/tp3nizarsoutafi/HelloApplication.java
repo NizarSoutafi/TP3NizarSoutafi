5b28bf6e6fa5a3c65fab30ed20580a73ec4ef0c4
Nizar Soutafi <SOUTAFI_NIZAR@emsi-edu.ma>
2024-12-25 20:18:32 +0100

    commit

---
+++ src/main/java/ma/emsi/nizarsoutafi/tp3nizarsoutafi/HelloApplication.java
@@ -0,0 +1,8 @@
+package ma.emsi.nizarsoutafi.tp3nizarsoutafi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+}
